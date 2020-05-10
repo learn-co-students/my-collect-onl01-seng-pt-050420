f9e75ddfb4aecc8f192852a077dbf31d503826ca
Ben Parish <parishben13@gmail.com>
2020-05-10 00:25:28 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+#any=['Sam Smith', 'Jessie Jackson', 'Shakira', 'J Lo']
+def my_collect(collection)
+  i=0 
+  woof = []
+  while i < collection.length
+  woof << yield(collection[i])
+  i+=1 
+ end
+ woof
+ end
+  
