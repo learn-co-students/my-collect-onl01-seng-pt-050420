89965613880ec73da4c68963047e16bbb910f11c
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-10 18:10:35 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,15 @@
+collection = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+def my_collect(collection)
+  i = 0 
+  array = []
+  while i < collection.length 
+  array << yield(collection[i])
+  i = i + 1
+end
+array
+end
 
+my_collect(collection) do |name|
+  name.split(" ").last
+end
 
