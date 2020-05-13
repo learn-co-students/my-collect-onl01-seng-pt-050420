43ebf77cb586171664861c3fad13568f2b82a50d
Hannah Sandusky <hnnahsandusky@gmail.com>
2020-05-13 01:23:46 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,20 +1,11 @@
-require 'pry'
+
 def my_collect
   array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
-  array = []
-  my_collect(array)
     my_collect(array) do |name|
       name.split(" ").first
   end
-
   collection = ["ruby", "javascript", "python", "objective-c"]
-  collection = []
   my_collect(collection) do |lang|
     lang.upcase
   end
-
-  end
 end
-
-> my_collect(["Tim", "Tom", "Jim"]) { |name| "#{name.first}" }
-> my_collect(["RUBY", "JAVASCRIPT", "PYTHON", "OBJECTIVE-C"]) { |lang| "#{lang.upcase}" }
