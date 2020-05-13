cb849d1ac9cc900ebcceeb12e332927b4fff0256
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 23:06:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,19 @@
+collection = ['Tim Jones', 'Tom Smith', 'Sophie Johnson', 'Antoin Miller']
 
+def my_collect(array) 
+ 
+ new_collect = []
+  i = 0 
+  while i < array.length
+  new_collect << yield(array[i])
+  i += 1 
+  
+end
+  # if i >= array.length
+  #   return new_collect.split(" ").downcase
+  # end
+
+new_collect
+  
+end
 
