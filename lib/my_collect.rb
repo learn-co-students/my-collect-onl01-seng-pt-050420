ac70307077ed53fec67330f398806f4e3bceff14
Michael Thomas <michaelthomas0611@gmail.com>
2020-05-15 21:09:12 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+require "pry"
 
-
+def my_collect(array)
+#  binding.pry
+  empty_array = [ ]
+  counter = 0
+  array.each do |x|
+    empty_array << yield(array[counter])
+    counter += 1
+  end
+empty_array
+end
