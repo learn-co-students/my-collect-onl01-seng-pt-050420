b6c288a03d235b0d345ea55dd26b7cd351821ac5
Deborah Santillan <s.debb@yahoo.com>
2020-05-17 19:01:11 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+collection = ["ruby", "javascript", "python", "objective-c"]
 
+def my_collect(collection)
+  i = 0 
+  newCollection = []
+  
+    while (i < collection.length) do 
+      yield collection[i]
+  
+  
+  newCollection.push(collection[i].upcase)
+    i += 1 
+
+  end
+  return newCollection
+  
+end
 
